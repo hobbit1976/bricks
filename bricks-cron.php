526f27aa82d24688ba302f83f1e74ffab9c20586
Holger <holger.kramer@arts.aero>
2013-09-19 11:36:09 +0200

    changed bill node

--- bricks-cron.php
+++ bricks-cron.php
@@ -1,12 +1,11 @@
 <?php
 
-watchdog('cron', 'Started cron');
-
 chdir('drupal');
 define('DRUPAL_ROOT', getcwd());
 
 include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+watchdog('cron', 'Started cron');
 
 drupal_cron_run();
 
